235ae9777cdad867f31926ac640954cd03790468
Farhana Bente Islam <103626684+farhanaiubat95@users.noreply.github.com>
2024-10-13 20:10:15 +0600

    create area of circle.c

---
+++ 4. Concept-Area of circle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    float  r,Area, pi=3.1416;
+    printf("Enter a radius value of circle =");
+    scanf("%f",&r);
+    Area= pi*r*r;
+
+    printf("The area of circle =%.3f",Area);
+
+getch();
+}
