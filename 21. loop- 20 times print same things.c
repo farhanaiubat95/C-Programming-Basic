c24a4b943902c61523b74cbde69f7275c8a98377
Farhana Bente Islam <21303195@iubat.edu>
2024-12-05 22:36:45 +0600

    create loop-20 times.c

---
+++ 21. loop- 20 times print same things.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int  i;
+    for(i=1;i<=20;i++)
+      {
+          printf("%d. My name is Farhana Bente Islam \n",i);
+          printf("%d. My Sister name is Faaria\n",i);
+      }
+
+}
