6861d92b1a62564f46e6c7e92a34892c7525f3f5
Farhana Bente Islam <21303195@iubat.edu>
2025-01-04 01:07:53 +0600

    Create all natural number

---
+++ 28. loop - all-natural numbers in reverse from n to 1..c
@@ -0,0 +1,13 @@
+//Print all-natural numbers in reverse from n to 1.
+#include<stdio.h>
+void main(){
+int i,n;
+printf("Input a number for reverse to 1 =  ",n);
+scanf("%d",&n);
+for(i=n; i>=1;i--)
+{
+    printf(" %d \n",i);
+
+}
+
+}
