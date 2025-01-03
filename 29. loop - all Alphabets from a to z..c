a7f1b3bb35162c92bbf006a91e360678c3361100
Farhana Bente Islam <21303195@iubat.edu>
2025-01-04 01:08:13 +0600

    create all alphabet.c

---
+++ 29. loop - all Alphabets from a to z..c
@@ -0,0 +1,13 @@
+//Print all Alphabets from a to z.
+#include<stdio.h>
+void main(){
+char i;
+
+        //for(i='A';i<='Z';i++)
+       //for(i=65;i<=90;i++)
+       for(i=97  ;i<=122  ;  i++)
+        {
+            printf("%c             ",i);
+        }
+getch();
+}
